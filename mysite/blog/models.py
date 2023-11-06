8eebb3065f7bb3e968e9bc9ef0731528c404d33b
It-opleidingalex <139441601+It-opleidingalex@users.noreply.github.com>
2023-11-06 17:19:51 +0100

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,16 +1,16 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-title = models.CharField(max_length=200)
-text = models.TextField()
-created_date = models.DateTimeField(default=timezone.now)
-published_date = models.DateTimeField(blank=True, null=True)
+class Voetbalspeler(models.Model):
+    naam_voetballer = models.CharField(max_length=100)
+    actuele_voetbalclub = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=100)
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
 
-def publish(self):
-        self.published_date = timezone.now()
+    def opslaan_en_publiceren(self):
+        self.datum_laatste_aanpassing = timezone.now()
         self.save()
 
-def __str__(self):
-        return self.title
+    def __str__(self):
+        return self.naam_voetballer
