f118f4e5934349e6ad081bd368d4a52d0ea282e7
It-opleidingalex <139441601+It-opleidingalex@users.noreply.github.com>
2023-11-06 17:27:14 +0100

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -19,16 +19,16 @@ from django.db import models
 from django.utils import timezone
 
 class Voetbalspeler(models.Model):
-    naam_voetballer = models.CharField(max_length=100)
-    actuele_voetbalclub = models.CharField(max_length=100)
-    auteur = models.CharField(max_length=100)
-    datum_invoer = models.DateTimeField(default=timezone.now)
-    datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
+        naam_voetballer = models.CharField(max_length=100)
+        actuele_voetbalclub = models.CharField(max_length=100)
+        auteur = models.CharField(max_length=100)
+        datum_invoer = models.DateTimeField(default=timezone.now)
+        datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
 
-    def opslaan_en_publiceren(self):
+def opslaan_en_publiceren(self):
         self.datum_laatste_aanpassing = timezone.now()
         self.save()
 
-    def __str__(self):
+def __str__(self):
         return self.naam_voetballer
 
